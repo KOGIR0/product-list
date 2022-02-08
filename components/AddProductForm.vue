0bdbf75d65d8e4a50cab45040d44d978b99a6b36
Igor Kondrahov <kondrahov.2000@mail.ru>
2022-02-08 16:05:35 +0300

    Added validation to AddProductForm

--- components/AddProductForm.vue
+++ components/AddProductForm.vue
@@ -1,20 +1,42 @@
 <template>
   <form>
-    <label>Наименование товара&#42;</label>
-    <input type="text" placeholder="Введите наименование товара" required>
+    <label>Наименование товара<span class="required">&#42;</span></label>
+    <input v-model="name" type="text" placeholder="Введите наименование товара" required>
     <label>Описание товара</label>
-    <textarea id="description" placeholder="Введите описание товара" />
-    <label>Ссылка на изображение товара&#42;</label>
-    <input type="text" placeholder="Введите ссылку" required>
-    <label>Цена товара&#42;</label>
-    <input type="number" required placeholder="Введите цену">
-    <button>Добавить товар</button>
+    <textarea id="description" v-model="description" placeholder="Введите описание товара" />
+    <label>Ссылка на изображение товара<span class="required">&#42;</span></label>
+    <input v-model="imgSrc" type="text" placeholder="Введите ссылку" required>
+    <label>Цена товара<span class="required">&#42;</span></label>
+    <input v-model="price" type="number" min="0" required placeholder="Введите цену">
+    <input type="submit" value="Добавить товар" :disabled="submitIsDisabled" :style="{background: submitColor}">
   </form>
 </template>
 
 <script>
 export default {
-  name: 'AddProductForm'
+  name: 'AddProductForm',
+  data () {
+    return {
+      name: '',
+      description: '',
+      imgSrc: '',
+      price: 0
+    }
+  },
+  computed: {
+    submitIsDisabled () {
+      if (!this.name || !this.price || !this.imgSrc) {
+        return true
+      }
+      return false
+    },
+    submitColor () {
+      if (!this.submitIsDisabled) {
+        return 'green'
+      }
+      return ''
+    }
+  }
 }
 </script>
 
@@ -36,11 +58,35 @@ input {
     box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
     border: none;
     border-radius: 4px;
-    color: #B4B4B4;
+    color: black;
     height: 36px;
     width: 100%;
     background-color: #FFFEFB;
     font-weight: 400;
+}
+
+input[type="submit"] {
+    margin-top: 24px;
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 15px;
+
+    text-align: center;
+    letter-spacing: -0.02em;
+}
+
+input[type="submit"]:hover:enabled {
+    cursor: pointer;
+}
+
+input[type="submit"]:enabled {
+    color: white;
+}
+
+input[type="submit"]:disabled {
+    color: #B4B4B4;
 }
 
 textarea {
@@ -50,7 +96,7 @@ textarea {
     box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
     border: none;
     border-radius: 4px;
-    color: #B4B4B4;
+    color: black;
     height: 36px;
     width: 100%;
     background-color: #FFFEFB;
@@ -61,7 +107,6 @@ form {
     box-shadow: 0px 20px 30px rgba(0, 0, 0, 0.04), 0px 6px 10px rgba(0, 0, 0, 0.02);
     border-radius: 4px;
     padding: 24px;
-    height: 440px;
 }
 
 button {
@@ -83,5 +128,9 @@ button {
 
 #description {
     height: 108px;
+}
+
+.required {
+    color: red;
 }
 </style>
